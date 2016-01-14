54eb9a7bde6a4e5381335fa733eaa3871ca004aa
kaflan <kaf_90@mail.ru>
2016-01-14 21:17:36 +0200

    step 10

--- public/app/scripts/directives/donat.js
+++ public/app/scripts/directives/donat.js
@@ -6,35 +6,35 @@
  * @description
  * # donat
  */
-function link(scope, el) {
-  var data = []; // тут наши буквы посчитаные
-  var color = d3.scale.category20b();
-
-  var el = el[0];
-  var width = el.clientWidth;
-  var height = el.clientHeight;
-  var min = Math.min(width, height);
-  // var arc = d3.svg.arc()
-  // .innerRadius(min / 2 * 0.5)
-  //   .outerRadius(min / 2 * 0.9);
-  var svg = d3.select(el).append('svg')
-    // .attr("width", width)
-    //  .attr("height", height)
-    //  .append("g")
-    //  .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-  var path = svg.selectAll("path")
-    // .data(pie(dataset.apples))
-    // .enter().append("path")
-    // .attr("fill", function(d, i) {
-    //   return color(i);
-    // })
-    // .attr("d", arc);
-  console.log('scope', scope.$watch(function(newValue,oldValue){
+function link(scope, el, attr) {
+  scope.$watch(function(newValue,oldValue){
     if(newValue.item){
-      console.log(letters(newValue.item.title));
+      var data = letters(newValue.item.title); // тут наши буквы посчитаные
+      var color = d3.scale.category20b();
+      var el = el[0];
+      var width = el.clientWidth;
+      var height = el.clientHeight;
+      var min = Math.min(width, height);
+      // var arc = d3.svg.arc()
+      // .innerRadius(min / 2 * 0.5)
+      //   .outerRadius(min / 2 * 0.9);
+      var svg = d3.select(el).append('svg')
+        // .attr("width", width)
+        //  .attr("height", height)
+        //  .append("g")
+        //  .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+      var path = svg.selectAll("path")
+        // .data(pie(dataset.apples))
+        // .enter().append("path")
+        // .attr("fill", function(d, i) {
+        //   return color(i);
+        // })
+        // .attr("d", arc);
+      console.log('scope', '\n', 'element', el);
     }
-  }), '\n', 'element', el);
-}
+    }
+  });
+
 function letters(str){
   var obj = {};
   var max = 1;
@@ -62,7 +62,9 @@ angular.module('publickApp')
     return {
       templateUrl: 'views/d3.html',
       restrict: 'E',
-      scope:{},
+      scope:{
+        text : "@"
+      },
       controller: 'DonatCtrl',
       link: link
     };
